cf073184d492d4b8dbd692efa7c35266fa1b3554
Yahya305 <saimyahya47@gmail.com>
2024-03-10 00:39:58 +0500

    removed redundant packages

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,18 @@ import "reflect-metadata";
 
 import dotenv from "dotenv";
 dotenv.config();
-import session from 'express-session';
-import SQLiteStore from 'connect-sqlite3';
 import Middlewares from "./middlewares/Middlewares";
 import ErrorMiddleware from "./middlewares/ErrorMiddleware";
 import { TestRouter } from "./routers/TestRouter";
 import { AuthRouter } from "./routers/AuthRouter";
+import expressSession from "express-session"
 import passport from "passport";
 
 const PORT = 8000;
 
 // Middlewares
 const app = Middlewares();
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(expressSession({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
